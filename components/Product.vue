7cbc838ef8f23a4a7f450decb25b6264e9719f5f
Lautaro Amengual <amengual.ln@gmail.com>
2024-09-10 21:45:04 -0300

    Mejor le sacamos lo de borrar

--- components/Product.vue
+++ components/Product.vue
@@ -1,5 +1,5 @@
 <template>
-  <article @pointerdown="handleDeleteProduct" @pointerup="handleDeleteAbort"
+  <article
     :class="`text-xl font-medium text-neutral-700 flex justify-between items-center bg-gray-100 p-4 rounded-xl select-none ${product.checked && 'opacity-75'}`">
     <h2 :class="product.checked && 'line-through opacity-50'">{{ product.name }}</h2>
     <div @click="handleProductCheck" class="bg-[#eee] rounded-full cursor-pointer">
@@ -20,18 +20,5 @@ const handleProductCheck = async () => {
   const prevValue = product.checked
   store.checkProduct(product.id) // Optimistic
   const { data, error } = await supabase.from('products').update({ checked: !prevValue, updated_at: new Date().toISOString() }).eq('id', product.id)
-}
-
-const handleDeleteProduct = async () => {
-  console.log('press started')
-  clearTimeout(deleteTimeout)
-  deleteTimeout = setTimeout(async () => {
-    store.deleteProduct(product.id)
-    const { deleteError } = await supabase.from('products').delete().eq('id', product.id)
-  }, 3000)
-}
-
-const handleDeleteAbort = () => {
-  clearTimeout(deleteTimeout)
 }
 </script>
